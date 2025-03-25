af25cdfc69e7fa09daf924d9268a5ed16395c975
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2025-03-25 07:20:01 +0000

    :pencil2: Add new zone; PHG07

--- scripts/fetcher.py
+++ scripts/fetcher.py
@@ -18,15 +18,13 @@ def fetch_data(relative_month: int):
     reqUrl = "https://www.e-solat.gov.my/index.php"
 
     jakim_code = [
-        "JHR01", "JHR02", "JHR03", "JHR04", "KDH01", "KDH02", "KDH03", "KDH04",
-        "KDH05", "KDH06", "KDH07", "KTN01", "KTN02", "MLK01", "NGS01", "NGS02", "NGS03",
-        "PHG01", "PHG02", "PHG03", "PHG04", "PHG05", "PHG06", "PRK01", "PRK02",
-        "PRK03", "PRK04", "PRK05", "PRK06", "PRK07", "PLS01", "PNG01", "SBH01",
-        "SBH02", "SBH03", "SBH04", "SBH05", "SBH06", "SBH07", "SBH08", "SBH09",
-        "SGR01", "SGR02", "SGR03", "SWK01", "SWK02", "SWK03", "SWK04", "SWK05",
-        "SWK06", "SWK07", "SWK08", "SWK09", "TRG01", "TRG02", "TRG03", "TRG04",
-        "WLY01", "WLY02"
-    ]  # Total 59
+        "JHR01", "JHR02", "JHR03", "JHR04", "KDH01", "KDH02", "KDH03", "KDH04", "KDH05", "KDH06", "KDH07",
+        "KTN01", "KTN02", "MLK01", "NGS01", "NGS02", "NGS03", "PHG01", "PHG02", "PHG03", "PHG04", "PHG05",
+        "PHG06", "PHG07", "PRK01", "PRK02", "PRK03", "PRK04", "PRK05", "PRK06", "PRK07", "PLS01", "PNG01",
+        "SBH01", "SBH02", "SBH03", "SBH04", "SBH05", "SBH06", "SBH07", "SBH08", "SBH09", "SGR01", "SGR02",
+        "SGR03", "SWK01", "SWK02", "SWK03", "SWK04", "SWK05", "SWK06", "SWK07", "SWK08", "SWK09", "TRG01",
+        "TRG02", "TRG03", "TRG04", "WLY01", "WLY02"
+    ]  # Total 60
 
     # for testing
     # jakim_code = ["JHR01", "JHR02"]
