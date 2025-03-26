9773e44b73f24979b0ff2eab691434484f9f7d59
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2025-03-26 00:34:48 +0000

    :sparkles: Refactor push_to_firebase function to handle multiple processed files

--- scripts/push_db.py
+++ scripts/push_db.py
@@ -6,7 +6,7 @@ from firebase_admin import firestore
 
 
 def push_to_firebase():
-    # Use a service account.
+    # Use a service account. Download from Firebase Console > Project Settings > Service Accounts
     cred = credentials.Certificate('adminsdk-creds.json')
 
     app = firebase_admin.initialize_app(cred)
@@ -16,33 +16,33 @@ def push_to_firebase():
     # Open *=*.processed.json file
     y = os.listdir("outputs")
 
-    # TODO: Maybe loop through all processed files in outputs folder
-    processed_file = [x for x in y if x.endswith(".processed.json")][0]
+    processed_files = [x for x in y if x.endswith(".processed.json")]
 
-    print(processed_file)
+    for processed_file in processed_files:
+        # extract month and year from filename
+        x = processed_file.split(".")[0]
+        month, year = x.split("-")
 
-    # extract month and year from filename
-    x = processed_file.split(".")[0]
-    month, year = x.split("-")
+        print(f"ℹ️ Want to push for {month} {year}")
 
-    print(f"ℹ️ Want to push for {month} {year}")
+        month_ref = db.collection('waktusolat').document(f'{year}').collection(month.upper())
 
-    month_ref = db.collection('waktusolat').document(f'{year}').collection(month.upper())
+        with open(f'outputs/{processed_file}') as f:
+            raw_data = json.load(f)
 
-    with open(f'outputs/{processed_file}') as f:
-        raw_data = json.load(f)
+        # iterate the zones
+        for zone in raw_data["processed"]:
+            zone_name = zone["zone"]
+            prayer_times = zone["prayerTime"]
 
-    # iterate the zones
-    for zone in raw_data["processed"]:
-        zone_name = zone["zone"]
-        prayer_times = zone["prayerTime"]
+            # push to firebase
+            z = month_ref.document(zone_name).set({"prayerTime": prayer_times})
+            print("🚀 Pushed", zone_name)
 
-        # push to firebase
-        z = month_ref.document(zone_name).set({"prayerTime": prayer_times})
-        print("🚀 Pushed", zone_name)
+        # record the last update data
+        year_ref = db.collection('waktusolat').document(f'{year}')
+        new_data = {"last_updated": {month.upper(): firestore.firestore.SERVER_TIMESTAMP }}
+        year_ref.set(new_data, merge=True)
 
-    # record the last update data
-    year_ref = db.collection('waktusolat').document(f'{year}')
-    new_data = {"last_updated": {month.upper(): firestore.firestore.SERVER_TIMESTAMP }}
-    year_ref.set(new_data, merge=True)
-
+if __name__ == "__main__":
+    push_to_firebase();
