69b1df3d964165d4c6364adc450ffed3334f9304
fareez <iqmal3@outlook.com>
2025-10-20 08:48:23 +0800

    :sparkles: Implement fetch_zones function to retrieve JAKIM zone codes from API

--- scripts/fetcher.py
+++ scripts/fetcher.py
@@ -9,6 +9,18 @@ import requests
 import urllib3
 
 
+def fetch_zones():
+    """
+    Fetch JAKIM zone codes from the API
+    """
+    response = requests.get('https://api.waktusolat.app/zones', timeout=10)
+    response.raise_for_status()
+    zones_data = response.json()
+    jakim_codes = [zone['jakimCode'] for zone in zones_data]
+    print(f'✅ Fetched {len(jakim_codes)} zones from API')
+    return jakim_codes
+
+
 def fetch_data(relative_month: int):
     """
     Fetch data from the given relative month
@@ -17,14 +29,7 @@ def fetch_data(relative_month: int):
 
     reqUrl = "https://www.e-solat.gov.my/index.php"
 
-    jakim_code = [
-        "JHR01", "JHR02", "JHR03", "JHR04", "KDH01", "KDH02", "KDH03", "KDH04", "KDH05", "KDH06", "KDH07",
-        "KTN01", "KTN02", "MLK01", "NGS01", "NGS02", "NGS03", "PHG01", "PHG02", "PHG03", "PHG04", "PHG05",
-        "PHG06", "PHG07", "PRK01", "PRK02", "PRK03", "PRK04", "PRK05", "PRK06", "PRK07", "PLS01", "PNG01",
-        "SBH01", "SBH02", "SBH03", "SBH04", "SBH05", "SBH06", "SBH07", "SBH08", "SBH09", "SGR01", "SGR02",
-        "SGR03", "SWK01", "SWK02", "SWK03", "SWK04", "SWK05", "SWK06", "SWK07", "SWK08", "SWK09", "TRG01",
-        "TRG02", "TRG03", "TRG04", "WLY01", "WLY02"
-    ]  # Total 60
+    jakim_code = fetch_zones()
 
     data = {}
     data['jakim'] = []
